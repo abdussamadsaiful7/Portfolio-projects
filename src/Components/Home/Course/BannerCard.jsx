0e895c986c5f379c501b20171d3d396e0f7dfb3e
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-06-22 00:16:19 +0600

    added Course Card conponent

---
+++ src/Components/Home/Course/BannerCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Style.css'
+import pic1 from '../../../assets/skill_icon/hero1.png'
+import pic2 from '../../../assets/skill_icon/fifo2.jpg'
+import pic3 from '../../../assets/skill_icon/wsda.jpg'
+
+const BannerCard = () => {
+    return (
+        <div className='dark:bg-cyan-900 pb-24 pt-10'>
+            <h1 className='text-center text-2xl font-bold mb-5'>Courses</h1>
+            <div className=' md:px-14 grid md:grid-cols-3 gap-4 md:h-[200px]'>
+                <div className='BCard mt-5 mx-auto'>
+                    <div className='imgBox'>
+                        <img src={pic1} alt="pic" />
+                        <h1 className='text-lg  text-center font-bold'>Web Development Course</h1>
+                    </div>
+                    <div className='content'>
+                        <p className='font-semibold text-xs'>I completed a 6 month course from Programming Hero Academy, where I learned Boostrap, TailwindCSS, DaisyUI, JavaScript, React.Js, MongoDB, Node.Js, Firebase,  etc and did some personal projects. Below is it, hope you like it</p>
+                    </div>
+                </div>
+                <div className='BCard mt-5 mx-auto'>
+                    <div className='imgBox'>
+                        <img className='h-32' src={pic2} alt="pic" />
+                        <h1 className='text-lg  text-center font-bold'>
+                            Professional Back Office Service</h1>
+                    </div>
+                    <div className='content'>
+                        <p className='font-semibold text-xs'>I have completed a course on office management from Fifo tech. Fifo tech is a subsidiary of BACCO under Project SEIP of Bangladesh Government. <br/> Fifo tech, Vision tower, Kawran Bazar, Dhaka, Bangladesh.</p>
+                    </div>
+                </div>
+                <div className='BCard mt-5 mx-auto'>
+                    <div className='imgBox'>
+                        <img className='h-32' src={pic3} alt="pic" />
+                        <h1 className='text-lg  text-center font-bold'>English Spoken Course</h1>
+                    </div>
+                    <div className='content'>
+                        <p className='font-semibold text-xs'>I have completed a course on English Spoken from Fifo tech Under WSDA from New Zeland. Fifo tech is a subsidiary of BACCO under Project SEIP of Bangladesh Government. <br/> Fifo tech, Vision tower, Kawran Bazar, Dhaka, Bangladesh.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default BannerCard;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ import Projects from './Projects';
 
 import hero from '../../assets/hero.png'
 import Banner from './Banner';
+import BannerCard from './Course/BannerCard';
 
 
 
@@ -15,7 +16,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Skills></Skills>
-            <div className='md:mx-20'>
+            {/* <div className='md:mx-20'>
                 <h1 className='text-center text-xl font-semibold'>Professional Web development Course </h1>
                 <div className='md: flex items-center justify-between shadow-md hover:shadow-xl p-8 hover:border hover:border-yellow-400 mt-4 md:px-20' data-aos="fade-up" data-aos-duration="3000">
                     <img className='w-32 h-32' src={hero} alt="icon" />
@@ -25,7 +26,8 @@ const Home = () => {
                         <p>Bangladesh</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <BannerCard></BannerCard>
             <Projects></Projects>
         </div>
     );
