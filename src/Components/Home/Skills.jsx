f9548fa81c932932c7343cbcbebf9c2647d759ae
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-06-26 22:00:02 +0600

    added vanilla tilt.js card hover

--- src/Components/Home/Skills.jsx
+++ src/Components/Home/Skills.jsx
@@ -17,32 +17,41 @@ const Skills = () => {
             <div>
                 <div className='grid grid-cols-2 md:grid-cols-4 md:px-24'>
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="React JS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={react} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="React JS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={react} alt="icon" />
+                    </div>
 
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="JavaScript"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={java} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="JavaScript"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={java} alt="icon" />
+                    </div>
 
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="HTML"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={html} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="HTML"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={html} alt="icon" />
+                    </div>
 
                     <div className='mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="MongoDB"><img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={mongodb} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="MongoDB"><img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={mongodb} alt="icon" />
+                    </div>
                 </div>
                 <div className='grid grid-cols-2 md:grid-cols-5'>
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="CSS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={css} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="CSS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={css} alt="icon" />
+                    </div>
 
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="Node JS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={node} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="Node JS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={node} alt="icon" />
+                    </div>
 
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="Bootstrap"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={bootstrap} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="Bootstrap"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={bootstrap} alt="icon" />
+                    </div>
 
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="Firebase"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={firebase} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="Firebase"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={firebase} alt="icon" />
+                    </div>
 
                     <div className=' mx-auto rounded-full shadow-md hover:shadow-xl hover:shadow-yellow-600 transition duration-700 ease-in-out p-2 tooltip  tooltip-bottom' data-aos="fade-up"
-                        data-aos-duration="3000" data-tip="TailwindCSS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={tailwind} alt="icon" /></div>
+                        data-aos-duration="3000" data-tip="TailwindCSS"> <img className='w-28 h-28 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300' src={tailwind} alt="icon" />
+                    </div>
                 </div>
                 <div className='text-center mt-6'>
                     <Link to='/skillsDetails'><button className="btn btn-sm btn-outline btn-warning">Details</button></Link>
--- src/Components/Home/SkillsDetails.jsx
+++ src/Components/Home/SkillsDetails.jsx
@@ -13,110 +13,190 @@ import figma from '../../assets/skill_icon/figma.png'
 import vercel from '../../assets/skill_icon/vercel.png'
 import netlify from '../../assets/skill_icon/netlify.jpg'
 import github from '../../assets/skill_icon/github.png'
+import { Tilt } from 'react-tilt'
+const defaultOptions = {
+    reverse: false,  // reverse the tilt direction
+    max: 35,     // max tilt rotation (degrees)
+    perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed: 1000,   // Speed of the enter/exit transition
+    transition: true,   // Set a transition on enter/exit.
+    axis: null,   // What axis should be disabled. Can be X or Y.
+    reset: true,    // If the tilt effect has to be reset on exit.
+    easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 
 const SkillsDetails = () => {
     return (
         <div className='px-5 md:px-20 py-20'>
             <h1 className='text-center font-bold text-3xl mb-4'>My Skills__</h1>
-            <div className='grid md:grid-cols-2 gap-8'>
-                <div className='flex items-center justify-center space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={react} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>React js</h1>
-                        <p className='text-lg  font-serif'>React.js is a powerful JavaScript library for building user interfaces with component-based architecture.</p>
+            <div className='grid md:grid-cols-2 gap-10'>
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center space-x-4 shadow-xl rounded-lg p-4'
+                        data-aos="fade-right" data-aos-duration="3000">
+                        <img className='w-32 h-32' src={react} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>React js</h1>
+                            <p className='text-lg  font-serif'>React.js is a powerful JavaScript library for building user interfaces with component-based architecture.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={node} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Node js</h1>
-                        <p className='text-lg font-serif'>Node.js is a runtime environment that allows server-side JavaScript execution.</p>
+                </Tilt>
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={node} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Node js</h1>
+                            <p className='text-lg font-serif'>Node.js is a runtime environment that allows server-side JavaScript execution.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={express} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Express js</h1>
-                        <p className='text-lg font-serif'>Express.js is a minimalistic and flexible web application framework for Node.js.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-right" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={express} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Express js</h1>
+                            <p className='text-lg font-serif'>Express.js is a minimalistic and flexible web application framework for Node.js.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={mongodb} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>MongoDB</h1>
-                        <p className='text-lg font-serif'>MongoDB is a scalable and flexible NoSQL database that stores data in JSON.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={mongodb} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>MongoDB</h1>
+                            <p className='text-lg font-serif'>MongoDB is a scalable and flexible NoSQL database that stores data in JSON.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={html} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>HTML5</h1>
-                        <p className='text-lg font-serif'>HTML is the standard markup language for creating the structure and content of web pages.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-right" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={html} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>HTML5</h1>
+                            <p className='text-lg font-serif'>HTML is the standard markup language for creating the structure and content of web pages.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={css} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>CSS3</h1>
-                        <p className='text-lg font-serif'>CSS is a styling language used to describe the presentation and visual appearance of HTML elements.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={css} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>CSS3</h1>
+                            <p className='text-lg font-serif'>CSS is a styling language used to describe the presentation and visual appearance of HTML elements.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={tailwind} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Tailwind CSS</h1>
-                        <p className='text-lg font-serif'>Tailwind CSS is a utility-first CSS framework that provides a wide range of pre-built classes.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-right" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={tailwind} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Tailwind CSS</h1>
+                            <p className='text-lg font-serif'>Tailwind CSS is a utility-first CSS framework that provides a wide range of pre-built classes.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={bootstrap} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Bootstrap</h1>
-                        <p className='text-lg font-serif'>Bootstrap is a popular front-end framework that collection of pre-designed components. </p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={bootstrap} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Bootstrap</h1>
+                            <p className='text-lg font-serif'>Bootstrap is a popular front-end framework that collection of pre-designed components. </p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={firebase} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Firebase</h1>
-                        <p className='text-lg font-serif'>Firebase is a comprehensive backend-as-a-service platform by Google that offers a suite services.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-right" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={firebase} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Firebase</h1>
+                            <p className='text-lg font-serif'>Firebase is a comprehensive backend-as-a-service platform by Google that offers a suite services.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={vs} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>VS Code</h1>
-                        <p className='text-lg font-serif'>VS Code is a feature-rich source code editor that supports multiple programming languages.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={vs} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>VS Code</h1>
+                            <p className='text-lg font-serif'>VS Code is a feature-rich source code editor that supports multiple programming languages.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={figma} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Figma</h1>
-                        <p className='text-lg font-serif'>Figma is a collaborative and cloud-based design tool that empowers teams to create.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-right" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={figma} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Figma</h1>
+                            <p className='text-lg font-serif'>Figma is a collaborative and cloud-based design tool that empowers teams to create.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32' src={vercel} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Vercel</h1>
-                        <p className='text-lg font-serif'>Vercel's frontend cloud gives developers frameworks, workflows, and infrastructure to build a faster, more personalized web.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32' src={vercel} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Vercel</h1>
+                            <p className='text-lg font-serif'>Vercel's frontend cloud gives developers frameworks, workflows, and infrastructure to build a faster, more personalized web.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32 rounded-full' src={netlify} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Netlify</h1>
-                        <p className='text-lg font-serif'>Netlify is the platform developers love for building highly-performant and dynamic websites, e-commerce stores and web applications.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-right" data-aos-duration="3000" >
+                        <img className='w-32 h-32 rounded-full' src={netlify} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Netlify</h1>
+                            <p className='text-lg font-serif'>Netlify is the platform developers love for building highly-performant and dynamic websites, e-commerce stores and web applications.</p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4'>
-                    <img className='w-32 h-32 rounded-full' src={github} alt="icon" />
-                    <div>
-                        <h1 className='text-2xl font-bold'>Github</h1>
-                        <p className='text-lg font-serif'>GitHub is a code hosting platform for version control and collaboration.</p>
+                </Tilt>
+
+
+                <Tilt options={defaultOptions}>
+                    <div className='flex items-center justify-center  space-x-4 shadow-xl rounded-lg p-4 '
+                        data-aos="fade-left" data-aos-duration="3000" >
+                        <img className='w-32 h-32 rounded-full' src={github} alt="icon" />
+                        <div>
+                            <h1 className='text-2xl font-bold'>Github</h1>
+                            <p className='text-lg font-serif'>GitHub is a code hosting platform for version control and collaboration.</p>
+                        </div>
                     </div>
-                </div>
+                </Tilt>
+
             </div>
         </div>
     );
